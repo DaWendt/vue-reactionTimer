3676299ad5091aa5c616bd4be5df7d4b30b55f0b
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-09-23 07:40:47 +0200

    add div and corresponding css to block.vue

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,4 +1,7 @@
 <template>
+    <div class="block">
+        click me
+    </div>
   
 </template>
 
@@ -9,5 +12,14 @@ export default {
 </script>
 
 <style>
+    .block{
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
 
 </style>
